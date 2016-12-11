b850a8fd8e16c3bb9731708dddb13ee1e7f88482
Fliptailser <fliptailser@gmail.com>
2016-12-11 17:46:15 -0500

    3rd song

--- game/hypersonic.py
+++ game/hypersonic.py
@@ -18,7 +18,7 @@ from player import *
 
 from kivy.clock import Clock as kivyClock
 
-SONG_NAMES = [ 'DanimalCannon_Axis', 'PROTODOME_ThisIsBLUESHIFT' ]
+SONG_NAMES = [ 'DanimalCannon_Axis', 'PROTODOME_ThisIsBLUESHIFT' , 'DaftPunk_HBFS']
 
 
 class MainWidget(BaseWidget) :
